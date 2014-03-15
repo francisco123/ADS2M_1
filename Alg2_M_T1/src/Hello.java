d2de96d0a74c3831a6fdd41c7e8dbfbf7ca33fe1
francisco123 <francisco_psn@hotmail.com>
2014-03-14 22:15:10 -0300

    Incrementando para classe HelloYou

--- Alg2_M_T1/src/Hello.java
+++ Alg2_M_T1/src/Hello.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 import static java.lang.System.out;
 public class Hello {
 
 	public static void main(String[] args){
-		out.println("Helloworld");
+		Scanner entrada=new Scanner(System.in);
+		out.println("Digite seu nome: ");
+		String nome=entrada.next();
+		out.println("Hello, "+nome);
 	}
 }
