5a4884baea57536e75844e53bb044bfb7f755b4a
francisco123 <francisco_psn@hotmail.com>
2014-03-17 20:01:50 -0300

    Implementando 2ª tentativa do Projeto pessoa: Criado Controller

--- Alg2_M_T2/src/com/senac/mvc/controller/PessoaController.java
+++ Alg2_M_T2/src/com/senac/mvc/controller/PessoaController.java
@@ -1,23 +1,51 @@
 package com.senac.mvc.controller;
-
+import java.util.Random;
 import com.senac.mvc.model.Pessoa;
 import com.senac.mvc.view.ContatoView;
 
 public class PessoaController {
 
-	private Pessoa contato;
-	
-	public void criarContato()
+	//Sortear Nomes
+	public String Nome()
 	{
-		contato = new Pessoa();
-		
-		contato.setNome("Rafael");
-		contato.setTelefone("555-1234");
+		Random rnomes=new Random();
+		String nomes[] = {"João","Francisco","Carlos","Vanissa","Vanessa","Carolina","Maria","Luiza","Rita"};
+		return nomes[rnomes.nextInt(10)];
 	}
 	
-	public void exibirContato( ContatoView view )
+	//Sortear Sobrenome
+	public String sobreNome()
 	{
-		view.printContato(contato.getNome(), contato.getTelefone());
+		Random rSobreNomes = new Random();
+		String sobreNomes[] = {"da Silva","Oselame","Borges","Almeida","de Freitas","Souza"};
+		return sobreNomes[rSobreNomes.nextInt(7)];
+	}
+	
+	//Sortear Endereco
+	public String Endereco()
+	{
+		Random rendereco = new Random();
+		String endereco[] = {"Bairro Santa Cecília","Av. Liberdade","Vila Repreza","Bairro São Lucas","Av. Ipiranga","Rua da Repreza"};
+		return endereco[rendereco.nextInt(7)];	
+	}
+	
+	//Sortear
+	public String Telefone()
+	{
+		Random rTelefone = new Random();
+		String telefone[] = {"93861081","86145778","94156584","81419845","93410800","94574134","81165489","96458535"};
+		return telefone[rTelefone.nextInt(9)];
+	}
+	
+	public void criaPessoa(){
+			Pessoa p = new Pessoa();
+			
+			for(int i=0;i < 10;i++);
+		
+			
+			
+			
+			
 	}
 	
 }
--- Alg2_M_T2/src/com/senac/mvc/model/Pessoa.java
+++ Alg2_M_T2/src/com/senac/mvc/model/Pessoa.java
@@ -4,16 +4,38 @@ public class Pessoa {
 	
 	private String nome;
 	private String telefone;
+	private String endereco;
+	private String telefoneTipo;
+	
+		//Getters and Setters
+	public String getEndereco(){
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco){
+		this.endereco = endereco;
+	}
+	
+	public String getTelefoneTipo() {
+		return telefoneTipo;
+	}
+	
+	public void setTelefoneTipo(String telefoneTipo) {
+		this.telefoneTipo = telefoneTipo;
+	}
 	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getTelefone() {
 		return telefone;
 	}
+	
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
